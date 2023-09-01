72fd275d8259678b2d49d244d3edf1ff4fd98ef0
Giomy8 <giomarmesa@gmail.com>
2023-08-31 21:12:17 -0500

    rutas de Home y NavBar en App.jsx GM

--- frontWellNestClinic/src/App.jsx
+++ frontWellNestClinic/src/App.jsx
@@ -1,15 +1,19 @@
 /* eslint-disable no-unused-vars */
 import React from 'react';
 import LandingPage from './components/LandingPage/LandingPage';
-import {Route, Routes} from "react-router-dom";
+import Home from './pages/Home/Home';
+import NavBar from './components/NavBar/NavBar';
+import {Route, Routes, useLocation} from "react-router-dom";
 import './App.css'
 
 function App() {
-
+  const { pathname } = useLocation();
   return (
     <div>
+      {pathname!='/' && <NavBar/>}
       <Routes>
         <Route path='/' element={<LandingPage/>}/>
+        <Route path="/home" element={<Home/>} />
       </Routes>
     </div>
   )
