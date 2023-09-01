ae752ffb460d792679e6676a0b13687d269e836c
Giomy8 <giomarmesa@gmail.com>
2023-08-31 22:44:26 -0500

    actions y reducer GM

---
+++ frontWellNestClinic/src/redux/actions.js
@@ -0,0 +1,30 @@
+import axios from 'axios';
+
+export const SET_USER = 'SET_USER';
+export const UPDATE_PROFILE_PICTURE = 'UPDATE_PROFILE_PICTURE';
+export const UPDATE_PASSWORD = 'UPDATE_PASSWORD';
+export const PAY_MEMBERSHIP = 'PAY_MEMBERSHIP';
+export const CANCEL_MEMBERSHIP = 'CANCEL_MEMBERSHIP';
+
+export const setUser = (user) => ({
+  type: SET_USER,
+  payload: user,
+});
+
+export const updateUserProfilePicture = (newProfilePicture) => ({
+  type: UPDATE_PROFILE_PICTURE,
+  payload: newProfilePicture,
+});
+
+export const updateUserPassword = (newPassword) => ({
+  type: UPDATE_PASSWORD,
+  payload: newPassword,
+});
+
+export const payMembership = () => ({
+  type: PAY_MEMBERSHIP,
+});
+
+export const cancelMembership = () => ({
+  type: CANCEL_MEMBERSHIP,
+});
--- frontWellNestClinic/src/redux/reducer.js
+++ frontWellNestClinic/src/redux/reducer.js
@@ -1,5 +1,43 @@
-const rootReducer =()=>{
+import { SET_USER, UPDATE_PROFILE_PICTURE, UPDATE_PASSWORD, PAY_MEMBERSHIP, CANCEL_MEMBERSHIP } from './actions'; 
 
+const initialState = {
+  user: null,
+  profilePicture: [], 
+  password: '', 
+  membershipStatus: [],
+  page:1,
 };
 
-export default rootReducer;
+const userReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case SET_USER:
+      return {
+        ...state,
+        user: action.payload,
+      };
+    case UPDATE_PROFILE_PICTURE:
+      return {
+        ...state,
+        profilePicture: action.payload,
+      };
+    case UPDATE_PASSWORD:
+      return {
+        ...state,
+        password: action.payload,
+      };
+    case PAY_MEMBERSHIP:
+      return {
+        ...state,
+        membershipStatus: 'Active',
+      };
+    case CANCEL_MEMBERSHIP:
+      return {
+        ...state,
+        membershipStatus: 'Expired',
+      };
+    default:
+      return state;
+  }
+};
+
+export default userReducer;
